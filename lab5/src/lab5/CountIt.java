7baf6aded40b7d5778f378ff741880191bca748b
Atone Joryman <jorymana@mail.gvsu.edu>
2018-02-27 20:49:16 -0500

    Add files via upload

---
+++ lab5/src/lab5/CountIt.java
@@ -0,0 +1,25 @@
+package lab5;
+
+public class CountIt {
+	private long fib(int n) {
+		if (n < 3) {
+			return 1;
+		}
+		else { 
+			return fib(n - 1) + fib (n - 2);
+		
+		}
+	}
+	public static void main(String[] args) {
+		CountIt r = new CountIt();
+		Long t = System.currentTimeMillis();
+		System.out.println(r. fib (20));
+		System.out.println ("Time:" + (System.currentTimeMillis() - t));
+	}
+}
+
+
+
+
+
+
